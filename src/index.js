54db746107a24a271c1edb6b7f56e17b66290961
rkxie <rkxie@amarsoft.com>
2018-08-27 10:51:03 +0800

    update webpack config file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import _ from 'loadsh';
+import printMe from './print';
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
   element.innerHTML = _.join(['hello','webpack'],'');
+  btn.innerHTML = 'click me and check the console';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 document.body.appendChild(component());
