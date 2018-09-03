4102bc76047cdc4da3388fd4ed9dadc1ab3d43dc
rkxie <rkxie@amarsoft.com>
2018-09-03 15:09:04 +0800

    split webpack config file

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -7,29 +6,13 @@ module.exports = {
   entry: {
     app:path.resolve(__dirname,'./src/index.js'),
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname,'./dist'),
-    hot: true
-  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({title:'Output Management'}),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
   ],
   output: {
     path: path.resolve(__dirname,'dist'),
     filename: '[name].bundle.js',
     publicPath: '/'
-  },
-  module:{
-    rules: [{
-      test:/\.css$/,
-      use:['style-loader','css-loader']
-    },{
-      test:/\.(png|svg|jpg|gif|jpeg)/,
-      use: ['file-loader']
-    }]
   }
 };
---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common,{
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname,'./dist'),
+    hot: true
+  },
+});
